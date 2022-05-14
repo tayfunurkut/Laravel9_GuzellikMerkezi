2c83045346b63a362ab5a9a6838328a8acfd18d3
tayfun8 <tayfunurkut@yandex.com>
2022-05-14 10:39:16 +0300

    Home Page Dynamic Slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+        $sliderdata = Service::limit(4)->get();
+        return view('home.index', [
+            'sliderdata' => $sliderdata
+        ]);
     }
 }
--- resources/views/home/_slider.blade.php
+++ resources/views/home/_slider.blade.php
@@ -1,44 +1,19 @@
 <div id="slides" class="cover-slides">
 		<ul class="slides-container">
+			@foreach($sliderdata as $data)
 			<li class="text-center">
-				<img src="{{ asset('assets')}}/images/slider-01.jpg" alt="">
+				<img src="{{ Storage::url($data->image) }}" alt="">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
-							<h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
-							trends to see any changes in performance over time.</p>
+							<h1 class="m-b-20"><strong>{{$data->title}}</strong></h1>
+							<p class="m-b-40">{!! $data->detail !!}}</p>
 							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
 						</div>
 					</div>
 				</div>
 			</li>
-			<li class="text-center">
-				<img src="{{ asset('assets')}}/images/slider-02.jpg" alt="">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
-							trends to see any changes in performance over time.</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
-						</div>
-					</div>
-				</div>
-			</li>
-			<li class="text-center">
-				<img src="{{ asset('assets')}}/images/slider-03.jpg" alt="">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
-							trends to see any changes in performance over time.</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
-						</div>
-					</div>
-				</div>
-			</li>
+			@endforeach
 		</ul>
 		<div class="slides-navigation">
 			<a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
